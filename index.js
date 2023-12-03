e806c4cf8d57117c38b73688a8b38fbf82841f38
dipujafar <dipujafarmr@gmail.com>
2023-12-04 01:05:23 +0600

    added admin related apis

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 const express = require("express");
 const cors = require("cors");
 require("dotenv").config();
-const jwt = require('jsonwebtoken');
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const jwt = require("jsonwebtoken");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const app = express();
 const port = process.env.PORT || 5000;
 
@@ -10,20 +10,20 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-const verifyToken = (req, res, next)=>{
-  if(!req.headers.authorization){
-    return res.status(401).send({message: "unauthorized access"});
-  };
+const verifyToken = (req, res, next) => {
+  if (!req.headers.authorization) {
+    return res.status(401).send({ message: "unauthorized access" });
+  }
 
-  const token = req.headers.authorization.split(' ')[1];
+  const token = req.headers.authorization.split(" ")[1];
 
-  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded)=>{
-    if(err){
-      return res.status(401).send({message: "unauthorized access"});
+  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+    if (err) {
+      return res.status(401).send({ message: "unauthorized access" });
     }
     req.decoded = decoded;
     next();
-  })
+  });
 };
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@jafardipu.hwlq4pv.mongodb.net/?retryWrites=true&w=majority`;
@@ -46,31 +46,82 @@ async function run() {
     const reviewCollection = client.db("employee").collection("reviews");
     const userCollection = client.db("employee").collection("users");
 
-
-    // hr related api
-    app.get('/users/hr/:email', verifyToken, async(req, res)=>{
-      const email = req.params.email;
-      if(email !== req.decoded.email){
-        return res.status(403).send({message: "forbidden"});
-      }
-      
+    // admin middleware verification
+    const verifyAdmin = async(req,res,next)=>{
+      const email = req.decoded.email;
       const query = {email: email};
       const user = await userCollection.findOne(query);
-      let hr = false;
-      if(user){
-        hr = user?.role === "HR";
+      const isAdmin =  user?.role === "Admin" && user.verified == true;
+      if(!isAdmin){
+        return res.status(403).send({message: "forbidden"});
       };
-      res.send({hr});
-    })
+      next();
+    }
+
+    // hr middleware verification
+    const verifyHr = async (req, res, next) => {
+      const email = req.decoded.email;
+      const query = { email: email };
+      const user = await userCollection.findOne(query);
+      const isAdmin = user?.role === "HR" && user.verified == true;
+      if (!isAdmin) {
+        return res.status(403).send({ message: "forbidden" });
+      }
+      next();
+    };
+
+    // hr related api
+    app.get("/users/hr/:email", verifyToken, async (req, res) => {
+      try {
+        const email = req.params.email;
+        if (email !== req.decoded.email) {
+          return res.status(403).send({ message: "forbidden" });
+        }
+
+        const query = { email: email };
+        const user = await userCollection.findOne(query);
+        let hr = false;
+        if (user) {
+          hr = user?.role === "HR" && user.verified == true;
+        }
+        res.send({ hr });
+      } catch {
+        //
+      }
+    });
+
+    // admin related api
+    app.get("/users/admin/:email", verifyToken, async (req, res) => {
+      try {
+        const email = req.params.email;
+        if (email !== req.decoded.email) {
+          return res.status(403).send({ message: "forbidden" });
+        }
+
+        const query = { email: email };
+        const user = await userCollection.findOne(query);
+        let admin = false;
+        if (user) {
+          admin = user?.role === "Admin" && user.verified == true;
+        }
+        res.send({ admin });
+      } catch {
+        //
+      }
+    });
 
     //jwt related apis
-    app.post('/jwt', async(req,res)=>{
-      const user = req.body;
-      const token = jwt.sign(user,process.env.ACCESS_TOKEN_SECRET,{expiresIn: '1h'});
-      console.log({token});
-      res.send({token});
-    })
-
+    app.post("/jwt", async (req, res) => {
+      try {
+        const user = req.body;
+        const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {
+          expiresIn: "1h",
+        });
+        res.send({ token });
+      } catch {
+        //
+      }
+    });
     app.get("/service", async (req, res) => {
       try {
         const result = await serviceCollection.find().toArray();
@@ -89,10 +140,61 @@ async function run() {
       }
     });
 
-    app.get("/employees",verifyToken, async(req,res)=>{
-      const result = await userCollection.find().toArray();
+    // employee related api
+    app.get("/employees", verifyToken, async (req, res) => {
+      try {
+        const result = await userCollection.find().toArray();
+        res.send(result);
+      } catch {
+        //
+      }
+    });
+
+    app.post("/employees", async (req, res) => {
+      try {
+        const user = req.body;
+        const result = await userCollection.insertOne(user);
+        res.send(result);
+      } catch {
+        //
+      }
+    });
+
+    app.patch("/employees/:id", verifyToken, verifyHr, async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const updateDoc = {
+        $set: {
+          verified: true,
+        },
+      };
+      const result = await userCollection.updateOne(query, updateDoc);
       res.send(result);
-    })
+    });
+
+    app.patch("/employees/hr/:id", verifyToken, verifyAdmin, async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const updateDoc = {
+        $set: {
+          role: "HR",
+        },
+      };
+      const result = await userCollection.updateOne(query, updateDoc);
+      res.send(result);
+    });
+
+    app.patch("/employees/fire/:id", verifyToken, verifyAdmin, async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const updateDoc = {
+        $set: {
+          fired: true,
+        },
+      };
+      const result = await userCollection.updateOne(query, updateDoc);
+      res.send(result);
+    });
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log(
